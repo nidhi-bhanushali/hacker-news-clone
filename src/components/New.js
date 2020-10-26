1811b04aae0d01fc1c5e23eb0ec21b5a92a4b181
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2020-10-26 19:48:54 +0530

    Displayed New news Stories

--- src/components/New.js
+++ src/components/New.js
@@ -1,37 +1,62 @@
 import axios from 'axios';
+import ReactDOM from 'react-dom';
 import React , { useState , useEffect} from 'react'
+import NewArticles from './NewArticles';
+import Top from './Top';
+
 
 const New = () => {
 
     let [newArticlesId , setNewArticlesId] =  useState([]);
-    const [newArticle , setNewArticle] =  useState({});
+    let [newArticle , setNewArticle] =  useState([]);
 
     useEffect(() => {
+        let finalArticle = []
         const fetchNewArticlesId = async () => {
         try {
             const res = await axios('https://hacker-news.firebaseio.com/v0/newstories.json?orderBy="$key"&limitToFirst=30');
-            newArticlesId = [...res.data]
+            newArticlesId = [...res.data];
+            // setNewArticlesId(res.data)
             console.log(res.data);
             
             await Promise.all(
                         newArticlesId.map(async (id) => {
                             const response = await axios(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
-                            setNewArticle(response.data)
+                            newArticle = [response.data]
+                            let newArticle1 = (Object.entries(newArticle));
+                            console.log(newArticle1[0][1].url);
+                            console.log(newArticle1[0][1].title)
+                            let newArticleElTitle = newArticle1[0][1]
+                            // let newArticleElUrl = newArticle1[0][1].url
+                             finalArticle.push(newArticleElTitle)
+                             console.log(finalArticle)
+                            // setNewArticle(response.data)
+                            ReactDOM.render(
+                                <ul>
+                                    <NewArticles data = {finalArticle}/>
+                               </ul> ,
+                                document.getElementById('new')
+                            )
+                            
                             console.log(response.data);
                         })
                     )
         } catch (error) {
             console.log(error);
-        }    
+        } 
+           
     };
         fetchNewArticlesId();
     }, []);
 
+    
+
     return (
-        <div className = 'container'>
-            <h5>The page shows New articles</h5> 
+        <div className = 'container' id = 'new'>
+            
         </div>
     )
 }
 
 export default New
+
